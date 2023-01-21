68885432380bcb8ef766bb737b415912b666ded9
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-01-20 22:21:35 -0800

    answer of first challenge

--- answer.js
+++ answer.js
@@ -1,1 +1,35 @@
-console.log("sdfsfs")
+//capitalize(‘a short sentence’) → ‘A Short Sentence’
+let text1 = 'a short sentence';
+let splitText1 = text1.split(' ');
+
+let capSplitText1 = splitText1.map(word => {
+    return word[0].toUpperCase() + word.slice(1);
+})
+
+let joinCapSplitText1 = capSplitText1.join(" ");
+
+//console.log(joinCapSplitText1);
+
+// capitalize(‘a lazy fox’) → ‘A Lazy Fox’
+let text2 = 'a lazy fox';
+let splitText2 = text2.split(" ");
+
+let capSplitText2 = splitText2.map(word => {
+    return word[0].toUpperCase() + word.slice(1);
+})
+
+let joinCapSplitText2 = capSplitText2.join(" ");
+
+//console.log(joinCapSplitText2);
+
+// capitalize(‘look, it is working!’) → ‘Look, It Is Working!’
+let text3 = "look, it is working!"
+let splitText3 = text3.split(" ");
+
+let capSplitText3 = splitText3.map(word => {
+    return word[0].toUpperCase() + word.slice(1);
+})
+
+let joinCapSplitText3 = capSplitText3.join(" ");
+
+console.log(joinCapSplitText3);
